e2bd14987faa2fa49d3762224f033b69854c22d7
Briggs <c2094665@hallam.shu.ac.uk>
2023-11-24 12:19:37 +0000

    x

--- js/clock.js
+++ js/clock.js
@@ -17,4 +17,20 @@ setInterval(updateTime, 1000);
 updateTime();
 
 
+<div id="clockFace">
+              <div id="secondHand"></div>
+              <div id="minHand"></div>
+              <div id="hrHand"></div>
+              <div id="pin"></div>
+</div>
+
+const secondHand = document.getElementById("secondHand");
+const minHand = document.getElementById("minHand");
+const hrHand = document.getElementById("hrHand");
+hrHand.style.backgroundColor = '#0f0';
+<div id="secondHand" style="transform: rotate(12deg);"></div>
+
+const seconds = myDate.getSeconds();
+const rotSeconds = seconds * 6 - 90;
+
 })();
