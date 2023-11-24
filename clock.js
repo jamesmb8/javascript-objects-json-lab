8e4caa7426d4596276136cbaec7028b643e2891d
Briggs <c2094665@hallam.shu.ac.uk>
2023-11-24 11:45:19 +0000

    clock

--- clock.js
+++ clock.js
@@ -1,3 +1,8 @@
 (function(){ 
 console.info("Hello JS"); 
+
+const myNode = document.createElement("div");
+const digitalClock = document.getElementById("digitalClock");
+digitalClock.appendChild(myNode);
+
 })();
